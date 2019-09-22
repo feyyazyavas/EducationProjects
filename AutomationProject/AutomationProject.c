3be41bfe74b8a3171aaa717fc21c59d4e0c852fd
feyyaz yavaş <feyyazyavas@gmail.com>
2019-09-22 15:29:02 +0300

    Update AutomationProject.c

--- AutomationProject/AutomationProject.c
+++ AutomationProject/AutomationProject.c
@@ -6,41 +6,42 @@
 //Crystal - Tick Timer - RS232 Configuration
 #use delay(crystal=20000000)
 #use timer(timer=1,tick=1us,bits=32,NOISR)
-#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
+//!#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)
 
 //Setting Pins As Output
-#use FIXED_IO( C_outputs=PIN_C5 )
-#use FIXED_IO( A_outputs=PIN_A2,PIN_A1,PIN_A0 )
+#use FIXED_IO( C_outputs=PIN_C3 )
+#use FIXED_IO( A_outputs=PIN_A5,PIN_A2,PIN_A1,PIN_A0 )
 #use fast_io(b)
 
 //Input - Output Pins Definition
 #define Input1 PIN_A3
-#define Output1 PIN_A0
+#define Output1 PIN_A2
 #define Output2 PIN_A1
-#define Output3 PIN_A2
-#define Output_Pin   PIN_C5
+#define Output3 PIN_A0
+#define Output_Pin   PIN_C3
+#define Led PIN_A5
 
 //Sensor Input Pin Definition
-#define Sensor1_Pin  PIN_B4
+#define Sensor1_Pin  PIN_B6
 #define Sensor2_Pin  PIN_B5
-#define Sensor3_Pin  PIN_B6
+#define Sensor3_Pin  PIN_B4
 
 //Encoder Input Pin Definiton
-#define Encoder1_Pin1   PIN_B0
-#define Encoder1_Pin2   PIN_B1
-#define Encoder1_Pin3   PIN_B2
-#define Encoder1_Pin4   PIN_B3
+#define Encoder1_Pin1   PIN_B3
+#define Encoder1_Pin2   PIN_B2
+#define Encoder1_Pin3   PIN_B1
+#define Encoder1_Pin4   PIN_B0
 
-#define Encoder2_Pin1   PIN_C0
-#define Encoder2_Pin2   PIN_C1
-#define Encoder2_Pin3   PIN_C2
-#define Encoder2_Pin4   PIN_C3
+#define Encoder2_Pin1   PIN_C7
+#define Encoder2_Pin2   PIN_C6
+#define Encoder2_Pin3   PIN_C5
+#define Encoder2_Pin4   PIN_C4
 
-//Beginning Time Definition
-#define beginningTime 12500000
 
 
 unsigned int32 selectTick, selectCounter;
+unsigned int32 selectTick_Led, selectCounter_Led;
+int beginningTime;
 
 
 int encoderVal[5];
@@ -48,6 +49,7 @@ float encoderMap;
 int encoder1, encoder2;
 float highTime, lowTime;
 float previous_highTime, previous_lowTime;
+int previous_beginningTime;
 
 
 int outputState=true;
@@ -58,32 +60,57 @@ int sensor2Activity=false;
 int sensor3Activity=false;
 
 
-float period;
 int t1,t2,t3;
 int closeVal;
-
-
-float Val;
-int multipletion;
-float division;
+int counter=0;
+int pin1=false;
 
 
 #int_RB
 void  Sensor_Interrupt()
 {   
-   if((input(Sensor1_Pin) == 0) && (sensor1Activity == true))
+   if((input(Sensor1_Pin) == 0))
    {
       t1=0;
+      output_high(Output1);
+      sensor1Activity = true;
    }
    
-   if((input(Sensor2_Pin) == 0) && (sensor2Activity == true))
+   if((input(Sensor2_Pin) == 0))
    {
       t2=0;
+      output_high(Output2);
+      sensor2Activity = true;
    }
    
-   if((input(Sensor3_Pin) == 0) && (sensor3Activity == true))
+   if((input(Sensor3_Pin) == 0))
    {
       t3=0;
+      output_high(Output3);
+      sensor3Activity = true;
+   }
+}
+
+void Led_Control()
+{
+   if(get_ticks() - selectTick_Led >= 125000)
+   {
+      selectTick_Led = get_ticks();
+      selectCounter_Led++;
+   }
+   
+   if(selectCounter_Led > 0 && selectCounter_Led <= 4)
+   {          
+      output_high(Led);     
+   }
+   else if(selectCounter_Led > 4 && selectCounter_Led <= 8)
+   {  
+      output_low(Led);
+   }
+   
+   if(selectCounter_Led >= 8)
+   {
+      selectCounter_Led = 0;
    }
 }
 
@@ -144,10 +171,10 @@ int encoderGetValue(int8 pin1, int8 pin2, int8 pin3, int8 pin4)
 float encoderCalc()
 {
    encoder1 = encoderGetValue(Encoder1_Pin1, Encoder1_Pin2, Encoder1_Pin3, Encoder1_Pin4);
-   printf("encoder1:%d\r\n",encoder1);
+//!   printf("encoder1:%d\r\n",encoder1);
    
    encoder2 = encoderGetValue(Encoder2_Pin1, Encoder2_Pin2, Encoder2_Pin3, Encoder2_Pin4);
-   printf("encoder2:%d\r\n",encoder2);
+//!   printf("encoder2:%d\r\n",encoder2);
    
    if(encoder1 <= 6)
    {
@@ -155,48 +182,49 @@ float encoderCalc()
       {
          case 0:
          {
-            encoderMap = 20;
+            encoderMap = mapf(encoder2,0,15,32,16);
+            encoderMap = encoderMap*10;
             break;
          }
          
          case 1:
          {
-            encoderMap = mapf(encoder2,0,15,2,4);
+            encoderMap = mapf(encoder2,0,15,24,12);
             encoderMap = encoderMap*10;
             break;
          }
          
          case 2:
          {
-            encoderMap = mapf(encoder2,0,15,4,6);
+            encoderMap = mapf(encoder2,0,15,16,8);
             encoderMap = encoderMap*10;
             break;
          }
          
          case 3:
          {
-            encoderMap = mapf(encoder2,0,15,6,8);
+            encoderMap = mapf(encoder2,0,15,12,6);
             encoderMap = encoderMap*10;
             break;
          }
          
          case 4:
          {
-            encoderMap = mapf(encoder2,0,15,8,10);
+            encoderMap = mapf(encoder2,0,15,8,4);
             encoderMap = encoderMap*10;
             break;
          }
          
          case 5:
          {
-            encoderMap = mapf(encoder2,0,15,10,13);
+            encoderMap = mapf(encoder2,0,15,6,3);
             encoderMap = encoderMap*10;
             break;
          }
          
          case 6:
          {
-            encoderMap = mapf(encoder2,0,15,13,17);
+            encoderMap = mapf(encoder2,0,15,4,2);
             encoderMap = encoderMap*10;
             break;
          } 
@@ -205,6 +233,7 @@ float encoderCalc()
    else
    {
       encoderMap = 20;
+      beginningTime = 4;
    }
    return(encoderMap);
 }
@@ -212,58 +241,25 @@ float encoderCalc()
 
 void ClosingLoopCalc()
 {
-   period = (float)highTime*2;
-   period = (float)period/10;
-   
-   if(period > 10)
+   if(encoder1 < 3)
    {
-      closeVal = 1;
-      printf("kapanma:%d\r\n",CloseVal);
-   }
-   else if(period <= 10)
-   {
-      period = (float)period;
-      Val = (10/period);
-      multipletion = Val*10;
-      multipletion = multipletion%10;
-      
-      if(multipletion >= 5)
-      {
-         division = (float)multipletion/10;
-         Val = Val + division;
-         CloseVal=Val;
-         printf("kapanma:%d\r\n",CloseVal);
-      }
-      else if(multipletion < 5)
-      {
-         division = (float)multipletion/10;
-         Val = Val - division;
-         CloseVal=Val;
-         printf("kapanma:%d\r\n",CloseVal);
-      }     
-   }
-}
-
-
-void Pin2Control()
-{
-   if(input(Input1) == 1)
-   {
-      highTime = 20;
-      lowTime = 20;
-      t1 = 0;
-      t2 = 0;
-      t3 = 0;
+      closeVal = 2;
+      beginningTime = 2;
+      previous_beginningTime = beginningTime;
    }
    else
    {
-      highTime = previous_highTime;
-      lowTime = previous_lowTime;
+      closeVal = 3;
+      beginningTime = 3;
+      previous_beginningTime = beginningTime;
    }
 }
 
+
 void OutputControl(int16 _highTime, int16 _lowTime)
 {
+   Led_Control();
+   
    if(get_ticks() - selectTick >= 125000)
    {
       selectTick = get_ticks();
@@ -274,6 +270,7 @@ void OutputControl(int16 _highTime, int16 _lowTime)
    {          
       if(outputState == true)
       {  
+         counter++;
          if(sensor1Activity == true)
          {
             t1++;
@@ -288,9 +285,8 @@ void OutputControl(int16 _highTime, int16 _lowTime)
          {
             t3++;
          }
-         
-         output_high(Output_Pin);
          outputState = false;
+         output_high(Output_Pin);   
       }    
    }
    else if(selectCounter > _highTime && selectCounter <= (_highTime + _lowTime))
@@ -306,57 +302,64 @@ void OutputControl(int16 _highTime, int16 _lowTime)
 }
 
 
-void SensorActivityControl()
-{
-   if(input(Sensor1_Pin) == 0)
-   {
-      Sensor1Activity = true;
-   }
-   
-   if(input(Sensor2_Pin) == 0)
-   {
-      Sensor2Activity = true;
-   }
-   
-   if(input(Sensor3_Pin) == 0)
-   {
-      Sensor3Activity = true;
-   }
-}
-
-
 void Start()
 {
+   start:
    output_high(Output1);
    output_high(Output2);
    output_high(Output3);
    
    do
    {
-      Pin2Control();
       OutputControl(highTime, lowTime);
-      SensorActivityControl();
+      
+      if(input(Input1) == 0 && pin1==true)
+      {
+//!         printf("RESET:\r\n");
+         reset_cpu();
+         
+      }
+      
+      if(input(Input1) == 1 && pin1==false)
+      {
+         pin1 = true;
+         lowTime=10;
+         counter = 0;
+         beginningTime = 4;
+         selectCounter = 0;
+         selectTick = 0;
+                 
+         goto start;
+      }
    }
-   while(get_ticks() < beginningTime);
+   while(counter < beginningTime);
    
-   output_low(Output_Pin);
-   output_low(Output1);
-   output_low(Output2);
-   output_low(Output3);
 
    if(sensor1Activity == true)
    {
       output_high(Output1);
+   }
+   else
+   {
+      output_low(Output1);
    }
    
    if(sensor2Activity == true)
    {
       output_high(Output2);
    }
+   else
+   {
+      output_low(Output2);
+   }
 
    if(sensor3Activity == true)
    {
       output_high(Output3);
+   }
+   else
+   {
+      output_low(Output3);
    }
 
    selectCounter = 0;
@@ -368,19 +371,16 @@ void ClosingControl()
 {
    if(t1 > closeVal)
    {
-      sensor1Activity = false;
       output_low(Output1);
    }
    
    if(t2 > closeVal)
    {
-      sensor2Activity = false;
       output_low(Output2);
    }
    
    if(t3 > closeVal)
    {
-      sensor3Activity = false;
       output_low(Output3);
    }
 }
@@ -388,34 +388,52 @@ void ClosingControl()
 
 void main()
 {  
-   disable_interrupts(GLOBAL);
-   disable_interrupts(INT_RB);
-     
-   output_low(Output1);
-   output_low(Output2);
-   output_low(Output3);
+   enable_interrupts(GLOBAL);
+   enable_interrupts(INT_RB);
+   
    output_low(Output_Pin);
+   output_high(Led);
    
-   highTime = encoderCalc();
-   printf("highTime:%f\r\n",highTime);
+   highTime = 10;
+//!   printf("highTime:%f\r\n",highTime);
    
-   lowTime = highTime;
-   printf("lowTime:%f\r\n",lowTime);
+   lowTime = encoderCalc();
+//!   printf("lowTime:%f\r\n",lowTime);
    
    previous_highTime = highTime;
    previous_lowTime = lowTime;
       
    ClosingLoopCalc();
    
-   Start();
+   if(input(Input1) == 1 && pin1 == false)
+   {
+      pin1 = true;
+      lowTime=10;
+      counter = 0;
+      beginningTime = 4;
+   }
    
-   enable_interrupts(GLOBAL);
-   enable_interrupts(INT_RB);
+   Start();
    
    while(TRUE)
    {  
       OutputControl(highTime, lowTime);
       ClosingControl();
-      Pin2Control();
+      
+      if(input(Input1) == 0 && pin1==true)
+      {
+         reset_cpu();
+      }
+      
+      if(input(Input1) == 1 && pin1==false)
+      {
+         pin1 = true;
+         lowTime=10;
+         counter = 0;
+         beginningTime = 4;
+         selectCounter = 0;
+         selectTick = 0;
+         Start();
+      }
    }
 }
